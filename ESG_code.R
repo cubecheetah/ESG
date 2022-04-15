f9ae4a77d25187cb1b6305a27e96e6cbdc1878c1
Lili S <96663804+cubecheetah@users.noreply.github.com>
2022-04-15 14:44:34 +0200

    Add files via upload

--- ESG_code.R
+++ ESG_code.R
@@ -5,12 +5,6 @@ setwd("/Users/sn.lili/ESG")
 library(readxl)
 ESG <- read_excel("ESG_datadata.xlsx")
 
-# We don't need some variables values
-ESG$GC <- NULL
-ESG$Similarity <- NULL
-ESG$ROIC <- NULL
-ESG$ROE <- NULL
-
 # converting numeric values and factors
 ESG$ESG <- as.numeric(ESG$ESG)
 ESG$ROA <- as.numeric(ESG$ROA)
@@ -22,10 +16,30 @@ ESG$Continent <- as.factor(ESG$Continent)
 ESG$Sector <- as.factor(ESG$Sector)
 ESG$Country <- as.factor(ESG$Country)
 
+# Adding dummies for mandatory ESG reporting (after Krueger et al. 2021)
+ESG$mand <- ifelse(ESG$Country == "Argentina" | ESG$Country == "Australia" 
+                   | ESG$Country == "Austria"| ESG$Country == "Canada" | ESG$Country == "Chile" 
+                   | ESG$Country == "China" | ESG$Country == "Germany" | ESG$Country == "Greece"| 
+                           ESG$Country == "Hong Kong"| ESG$Country == "Hungary"| ESG$Country == "Indonesia"| 
+                           ESG$Country == "Ireland"| ESG$Country == "Italy"| ESG$Country == "India"|
+                           ESG$Country == "Malaysia"| ESG$Country == "Netherlands"
+                   | ESG$Country == "Norway" | ESG$Country == "Pakistan" | ESG$Country == "Peru"
+                   | ESG$Country == "Philippines" | ESG$Country == "Poland"| ESG$Country == "Portugal"
+                   | ESG$Country == "Singapore" | ESG$Country == "Slovenia" | ESG$Country == "South Africa"
+                   | ESG$Country == "Spain"| ESG$Country == "Turkey"| ESG$Country == "United Kingdom"
+                   | ESG$Country == "Taiwan, Province of China", 1, 0)
+
 # Corrplot
 library(corrplot)
-M <- cor(ESG[3:7]) # only usable if no columns were removed
+M <- cor(ESG[3:8]) # only usable if no columns were removed
 corrplot(M, method = 'number') 
+
+# We don't need some variables values
+ESG$GC <- NULL
+ESG$Similarity <- NULL
+ESG$ROIC <- NULL
+ESG$ROE <- NULL
+
 # Strong correlation between GC and ESG, but weak (positive) association between ROA and ESG
 # Almost 0 correlation between ROE, ROIC and the other variables
 
@@ -34,6 +48,7 @@ boxplot(ESG$ESG)
 ESG[ESG==0] <- NA # The 0s will be converted to NAs
 ESG <- ESG[complete.cases(ESG), ] # and removed from the df
 summary(ESG)
+
 # Data visualisation
 esquisse::esquisser() # There is a ROA outlier (AirAsia with -5000 ROA)
 
@@ -50,21 +65,25 @@ ggplot(ESG) +
  theme_minimal()
 
 ESG <- ESG[-c(29), ] # drop the 29th row as it is a negative ROA outlier
-
+boxplot(ESG$ESG) # like
 
 # Check distributions
 library("car")
 qqPlot(ESG$ESG) # Looks normal
 qqPlot(ESG$ROA) # Not normal
+qqPlot(ESG$emp_nr)
+qqPlot(log(ESG$emp_nr)) # good
 
 # KStest
 ks.test(ESG$ESG, "pnorm", mean=mean(ESG$ESG), sd=sd(ESG$ESG)) # p value is greater than 0.05 <- normally distributed
 ks.test(ESG$ROA, "pnorm", mean=mean(ESG$ROA), sd=sd(ESG$ROA)) # p value is smaller than 0.05 <- not normal
+ks.test(ESG$emp_nr, "pnorm", mean=mean(ESG$emp_nr), sd=sd(ESG$emp_nr)) # p value is smaller than 0.05 <- not normal distr
 
-hist(ESG$ROA) # we should transform it or remove outliers(?)
+boxplot(ESG$ROA)
+hist(ESG$ROA) # we should transform it ?
 
 # Descriptive statistics
-summary <- summary(ESG) # North america and Asia have the most data, there is only 1 for Australia and Africa (consider deleting them?)
+summary(ESG) # North america and Asia have the most data, there is only 1 for Australia and Africa (consider deleting them?)
 
 # Model
 
@@ -72,27 +91,12 @@ summary <- summary(ESG) # North america and Asia have the most data, there is on
 model_1 <- lm(ESG ~ ROA, data = ESG)
 summary(model_1)
 
-model_2 <- lm(ESG ~ ROA + Industry, data = ESG)
+model_2 <- lm(ESG ~ ROA + mand, data = ESG)
 summary(model_2)
 
-model_3 <- lm(ESG ~ ROA + Industry + Continent, data = ESG)
+model_3 <- lm(ESG ~ ROA + log(emp_nr) + Continent + Sector, data = ESG)
 summary(model_3)
-# The R^2 is still quite low idk what to do :/ add size of firm or age of firm (?) or sample
 
-
-
-# Adding dummies for mandatory ESG reporting (after Krueger et al. 2021)
-ESG$mand <- ifelse(ESG$Country == "Argentina" | ESG$Country == "Australia" 
-                   | ESG$Country == "Austria"| ESG$Country == "Canada" | ESG$Country == "Chile" 
-                   | ESG$Country == "China" | ESG$Country == "Germany" | ESG$Country == "Greece"| 
-                           ESG$Country == "Hong Kong"| ESG$Country == "Hungary"| ESG$Country == "Indonesia"| 
-                           ESG$Country == "Ireland"| ESG$Country == "Italy"| ESG$Country == "India"|
-                           ESG$Country == "Malaysia"| ESG$Country == "Netherlands"
-                   | ESG$Country == "Norway" | ESG$Country == "Pakistan" | ESG$Country == "Peru"
-                   | ESG$Country == "Philippines" | ESG$Country == "Poland"| ESG$Country == "Portugal"
-                   | ESG$Country == "Singapore" | ESG$Country == "Slovenia" | ESG$Country == "South Africa"
-                   | ESG$Country == "Spain"| ESG$Country == "Turkey"| ESG$Country == "United Kingdom"
-                   | ESG$Country == "Taiwan, Province of China", 1, 0)
 
 model_mand <- lm(ESG ~ ROA + mand + Industry + Continent, data = ESG)
 summary(model_mand) # it has negative explanatory power? why :O
@@ -113,3 +117,10 @@ summary(model_europe) # overspecified
 northam <- ESG[ which(ESG$Continent =='North America'), ]
 model_na <- lm(ESG ~ ROA + Sector + mand, data = northam)
 summary(model_na)
+
+
+
+
+
+
+
